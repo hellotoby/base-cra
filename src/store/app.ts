60efad1f8aa3296e2bf28959fbb40c68a814f749
Toby Vervaart <tobyvervaart@Tobys-MacBook-Pro.local>
2022-09-24 11:31:54 +1000

    Add redux toolkit and tailwind

---
+++ src/store/app.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface AppState {
+  value: number;
+};
+
+const initialState = {
+  value: 0,
+} as AppState;
+
+const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.value++
+    },
+    decrement(state) {
+      state.value--
+    },
+  },
+});
+
+export const { increment, decrement } = appSlice.actions;
+export default appSlice.reducer;
