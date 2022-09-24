6fe58599eb29f50fde11e3d61251663a62e184a2
Toby Vervaart <tobyvervaart@Tobys-MacBook-Pro.local>
2022-09-24 11:43:45 +1000

    Add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import { store } from './store/store';
 import App from './App';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <></>,
+  }
+]);
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -11,7 +23,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
